dc5b6bc7c027b46a39ac9a63b9da38e2ea3b7fb9
Sathish Kumar <csathishkumar18@gmail.com>
2023-01-19 14:09:59 +0000

    Initial commit.

---
+++ CustomJsonFormatter/Main.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Globalization;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using Kbg.NppPluginNET.PluginInfrastructure;
+
+namespace Kbg.NppPluginNET
+{
+    class Main
+    {
+        internal const string PluginName = "Custom Json Formatter";
+        static string iniFilePath = null;
+        static bool someSetting = false;
+        static frmMyDlg frmMyDlg = null;
+        static int idMyDlg = -1;
+        static Bitmap tbBmp = Properties.Resources.star;
+        static Bitmap tbBmp_tbTab = Properties.Resources.star_bmp;
+        static Icon tbIcon = null;
+
+        public static void OnNotification(ScNotification notification)
+        {
+        }
+
+        internal static void CommandMenuInit()
+        {
+            StringBuilder sbIniFilePath = new StringBuilder(Win32.MAX_PATH);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, (uint)NppMsg.NPPM_GETPLUGINSCONFIGDIR, Win32.MAX_PATH, sbIniFilePath);
+            iniFilePath = sbIniFilePath.ToString();
+            if (!Directory.Exists(iniFilePath)) Directory.CreateDirectory(iniFilePath);
+            iniFilePath = Path.Combine(iniFilePath, PluginName + ".ini");
+            someSetting = (Win32.GetPrivateProfileInt("SomeSection", "SomeKey", 0, iniFilePath) != 0);
+
+            PluginBase.SetCommand(0, "Remove Newline Characters", RemoveNewLineCharacters, new ShortcutKey(false, false, false, Keys.None));
+            PluginBase.SetCommand(0, "Remove Newline Characters And Format JSON", RemoveNewLineCharactersAndFormat, new ShortcutKey(false, false, false, Keys.None));
+        }
+
+        internal static void SetToolBarIcon()
+        {
+            toolbarIcons tbIcons = new toolbarIcons();
+            tbIcons.hToolbarBmp = tbBmp.GetHbitmap();
+            IntPtr pTbIcons = Marshal.AllocHGlobal(Marshal.SizeOf(tbIcons));
+            Marshal.StructureToPtr(tbIcons, pTbIcons, false);
+            Win32.SendMessage(PluginBase.nppData._nppHandle, (uint)NppMsg.NPPM_ADDTOOLBARICON, PluginBase._funcItems.Items[idMyDlg]._cmdID, pTbIcons);
+            Marshal.FreeHGlobal(pTbIcons);
+        }
+
+        internal static void PluginCleanUp()
+        {
+            Win32.WritePrivateProfileString("SomeSection", "SomeKey", someSetting ? "1" : "0", iniFilePath);
+        }
+
+        internal static void RemoveNewLineCharacters()
+        {
+            string modifiedString = string.Empty;
+            IntPtr currentScint = PluginBase.GetCurrentScintilla();
+            ScintillaGateway scintillaGateway = new ScintillaGateway(currentScint);
+
+            try
+            {
+                int length = scintillaGateway.GetLength();
+                string allText = scintillaGateway.GetText(length + 1);
+                if (!string.IsNullOrWhiteSpace(allText))
+                {
+                    modifiedString = Regex.Replace(allText, @"\\", string.Empty, RegexOptions.None);
+                    modifiedString = Regex.Replace(modifiedString, @"rn", string.Empty, RegexOptions.None);
+                    scintillaGateway.SelectAll();
+                    scintillaGateway.ReplaceSel(modifiedString);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        internal static void RemoveNewLineCharactersAndFormat()
+        {
+            string modifiedString = string.Empty;
+            IntPtr currentScint = PluginBase.GetCurrentScintilla();
+            ScintillaGateway scintillaGateway = new ScintillaGateway(currentScint);
+
+            try
+            {
+                int length = scintillaGateway.GetLength();
+                string allText = scintillaGateway.GetText(length + 1);
+                modifiedString = Regex.Replace(allText, @"\\", string.Empty, RegexOptions.None);
+                modifiedString = Regex.Replace(modifiedString, @"rn", string.Empty, RegexOptions.None);
+                modifiedString = FormatOutput(modifiedString);
+                scintillaGateway.SelectAll();
+                scintillaGateway.ReplaceSel(modifiedString);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        internal static string FormatOutput(string jsonString)
+        {
+            var stringBuilder = new StringBuilder();
+
+            bool escaping = false;
+            bool inQuotes = false;
+            int indentation = 0;
+
+            foreach (char character in jsonString)
+            {
+                if (escaping)
+                {
+                    escaping = false;
+                    stringBuilder.Append(character);
+                }
+                else
+                {
+                    if (character == '\\')
+                    {
+                        escaping = true;
+                        stringBuilder.Append(character);
+                    }
+                    else if (character == '\"')
+                    {
+                        inQuotes = !inQuotes;
+                        stringBuilder.Append(character);
+                    }
+                    else if (!inQuotes)
+                    {
+                        if (character == ',')
+                        {
+                            stringBuilder.Append(character);
+                            stringBuilder.Append("\r\n");
+                            stringBuilder.Append('\t', indentation);
+                        }
+                        else if (character == '[' || character == '{')
+                        {
+                            stringBuilder.Append(character);
+                            stringBuilder.Append("\r\n");
+                            stringBuilder.Append('\t', ++indentation);
+                        }
+                        else if (character == ']' || character == '}')
+                        {
+                            stringBuilder.Append("\r\n");
+                            stringBuilder.Append('\t', --indentation);
+                            stringBuilder.Append(character);
+                        }
+                        else if (character == ':')
+                        {
+                            stringBuilder.Append(character);
+                            stringBuilder.Append('\t');
+                        }
+                        else if (!Char.IsWhiteSpace(character))
+                        {
+                            stringBuilder.Append(character);
+                        }
+                    }
+                    else
+                    {
+                        stringBuilder.Append(character);
+                    }
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
